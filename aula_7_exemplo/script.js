3012d0129ed27638c3c546265fd8cce6495b2b3f
Pietro Del Corona <pietrogdelcorona@gmail.com>
2023-04-26 14:43:53 -0300

    Em Manutenção

--- aula_7_exemplo/script.js
+++ aula_7_exemplo/script.js
@@ -6,17 +6,12 @@ function ler(){
             document.write("Dados: " + this.responseText + "<hr>");
             var dadosXml = this.responseXML;
             var produtos = dadosXml.getElementsByTagName("produto");
-            document.write("<table border='1'>");
-            document.write("     <tr> ");
-            document.write("   <th>Id</th>");
-            document.write("   <th>Nome</th>");
-            document.write("   <th>Preço</th>");
-            document.write("    </tr>");
+            document.getElementById("divTab").innerHTML            
             for (i = 0; i < produtos.length; i++) {
                 document.write("   <tr>");
                 var id = produtos[i].getElementsByTagName("id");
-                var id = produtos[i].getElementsByTagName("nome");
-                var id = produtos[i].getElementsByTagName("preco");
+                var nome = produtos[i].getElementsByTagName("nome");
+                var preco = produtos[i].getElementsByTagName("preco");
                 document.write("     <td>" + id[0].childNodes[0].nodeValue + "</td>");
                 document.write("     <td>" + nome[0].childNodes[0].nodeValue + "</td>");
                 document.write("     <td>" + preco[0].childNodes[0].nodeValue + "</td>");
